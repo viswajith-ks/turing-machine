6f8ee0b9e1e31df9aa65b84bb8c263b4104dd929
Viswajith K S <72287622+PsycoZombie@users.noreply.github.com>
2024-08-09 14:25:58 +0530

    beautification

--- State.java
+++ State.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-class State{
-	public boolean isfinal;
-	State[] neighbors;
-	char[] write;
-	char[] move;
-	public State(){
-		isfinal=false;
-		neighbors=new State[3];
-		write=new char[3];
-		move=new char[3];
-	}	
+class State {
+    public boolean isfinal;
+    State[] neighbors;
+    char[] write;
+    char[] move;
+    public State() {
+        isfinal = false;
+        neighbors = new State[3];
+        write = new char[3];
+        move = new char[3];
+    }
 }
